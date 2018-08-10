870f6e28f1f34135e52a94ba471d0c5a619e8fce
Honey1911 <honeysinghkv@gmail.com>
2018-08-10 16:56:48 +0530

    initial commit

---
+++ p18.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int i,j;
+	for(i=65;i<=69;i++)
+	{
+		for(j=65;j<=69;j++)
+		{
+			if(j<=134-i)
+			printf("%c",i);
+		}
+		printf("\n");
+	}
+	return 0;
+}
---
+++ p8.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int i,j;
+	for(i=69;i>=65;i--)
+	{
+		for(j=69;j>=65;j--)
+		{
+			printf("%c",i);
+			
+		}
+		printf("\n");
+	}
+	return 0;
+}
